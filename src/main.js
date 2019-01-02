245f220572aacde7877f3c80e3ac513eca1b199b
Alejandro Vivas <alejandrojv17@gmail.com>
2019-01-02 09:56:08 -0300

    CSS Fixes

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ firebase.initializeApp(config)
 //Vendors
 import 'vanilla-antd-message/dist/style.css'
 import 'bulma/css/bulma.css'
-import 'bulma-pageloader/dist/css/bulma-pageloader.min.css'
 
 Vue.config.productionTip = false
 
